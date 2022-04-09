d2d6aa20ce48b3610f6cc7092e995b4453bf5b7a
Yanick Egli <nick.eglirohr@gmail.com>
2022-04-09 17:19:17 +0200

    alex lee tutorials

---
+++ projects/alexlee_tutorial/JavaTutorial.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class JavaTutorial {
+    public static void main(String[] args) {
+        // System.out.print("A Sentance");
+        // System.out.print("B Sentance");
+
+        // String adjective = "beautiful";
+        // String verb = "puke";
+        // String noun = "dog";
+
+        // System.out.println("My " + adjective + " sister accidentally " + verb + "d all over her new " + noun + ".");
+
+        // All Escape Characters \\ \n \t \"
+        
+        Scanner scan = new  Scanner(System.in);
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon1 = scan.nextLine();
+
+        System.out.println("Enter pokemon 2:");
+        String pokemon2 = scan.nextLine();
+
+        System.out.println("Enter pokemon 3:");
+        String pokemon3 = scan.nextLine();
+
+        System.out.println("Enter pokemon 4:");
+        String pokemon4 = scan.nextLine();
+
+        System.out.println("Enter pokemon 5:");
+        String pokemon5 = scan.nextLine();
+
+        System.out.println("Enter pokemon 6:");
+        String pokemon6 = scan.nextLine();
+
+        System.out.println(pokemon1);
+
+// Tutorial Part https://www.youtube.com/watch?v=yvWnj_HfG6s&list=PL59LTecnGM1NRUyune3SxzZlYpZezK-oQ&index=10
+
+    }
+}
